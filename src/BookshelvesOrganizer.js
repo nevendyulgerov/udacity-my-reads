e648e9a72c0d80f8e3de71cb485ae74d5d17bc6e
Neven Dyulgerov <neven.diulgerov@gmail.com>
2017-11-07 14:00:48 +0200

    Extend component BookshelvesOrganizer

--- src/BookshelvesOrganizer.js
+++ src/BookshelvesOrganizer.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Bookshelf from './Bookshelf';
+import * as utils from './utils';
 
 class BookshelvesOrganizer extends React.Component {
+  // TODO: Add type checking
+
   render = () => (
     <div className="list-books">
       <div className="list-books-title">
-        <h1>{this.props.title}</h1>
+        <h1>My Reads</h1>
       </div>
       <div className="list-books-content">
         <div>
           {this.props.bookshelves.map(bookshelf => (
-            <Bookshelf key={bookshelf} title={bookshelf.split('-').map(word => word.charAt(0).toUpperCase()+word.slice(1)).join(' ')} books={this.props.books} name={bookshelf}/>
+            <Bookshelf
+              key={bookshelf}
+              name={bookshelf.name}
+              title={utils.titlize(bookshelf)}
+              bookshelves={this.props.bookshelves}
+              books={this.props.books.filter(book => book.shelf === utils.toCamelCase(bookshelf))}
+              updateBook={this.props.updateBook}
+            />
           ))}
         </div>
       </div>
