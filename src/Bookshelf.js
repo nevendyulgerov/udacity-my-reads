8f3ed3680e9fe4cc439f776d479a87a466ce1b32
Neven Dyulgerov <neven.diulgerov@gmail.com>
2017-11-07 14:00:36 +0200

    Extend component Bookshelf

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -2,17 +2,29 @@ import React from 'react';
 import Book from './Book';
 
 class Bookshelf extends React.Component {
+  // TODO: Add type checking
+
   render = () => (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{this.props.title}</h2>
       <div className="bookshelf-books">
-        <ol className="books-grid">
-          {this.props.books.map(book => (
-            <li key={book.id}>
-              <Book title={book.title} authors={book.authors} smallThumbnail={book.imageLinks.smallThumbnail}/>
-            </li>
-          ))}
-        </ol>
+        {this.props.books.length > 0 && (
+          <ol className="books-grid">
+            {this.props.books.map(book => (
+              <li key={book.id}>
+                <Book
+                  id={book.id}
+                  title={book.title}
+                  authors={book.authors}
+                  shelf={book.shelf}
+                  smallThumbnail={book.imageLinks.smallThumbnail}
+                  bookshelves={this.props.bookshelves}
+                  updateBook={this.props.updateBook}
+                />
+              </li>
+            ))}
+          </ol>
+        )}
       </div>
     </div>
   );
