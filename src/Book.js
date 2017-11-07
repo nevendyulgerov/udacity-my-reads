cc8038b187cb010efa571961c138dee8af3c8c19
Neven Dyulgerov <neven.diulgerov@gmail.com>
2017-11-07 14:00:21 +0200

    Extend component Book

--- src/Book.js
+++ src/Book.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import * as utils from './utils';
 
 class Book extends React.Component {
+  // TODO: Add type checking
+
   render = () => (
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.smallThumbnail})` }}></div>
         <div className="book-shelf-changer">
-          <select>
+          <select defaultValue={this.props.shelf || 'none'} onChange={(e) => this.props.updateBook({id: this.props.id}, e.target.value)}>
             <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
+            {this.props.bookshelves.map(bookshelf => (
+              <option key={bookshelf} value={utils.toCamelCase(bookshelf)}>{utils.titlize(bookshelf)}</option>
+            ))}
             <option value="none">None</option>
           </select>
         </div>
       </div>
-      <div className="book-title">{this.props.title}</div>
-      <div className="book-authors">{this.props.authors.join(', ')}</div>
+      <div className="book-title">{this.props.title && this.props.title}</div>
+      <div className="book-authors">{this.props.authors && this.props.authors.join(', ')}</div>
     </div>
   );
 }
